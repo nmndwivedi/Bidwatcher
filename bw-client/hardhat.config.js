100b9d0adf81abc2cf735fe05fb0ec14c21c253a
Naman <nmndwivedi@gmail.com>
2022-04-24 00:29:34 +0200

    database setup

--- bw-client/hardhat.config.js
+++ bw-client/hardhat.config.js
@@ -24,11 +24,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.NEXT_PUBLIC_MUMBAI_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    mainnet: {
+      url: process.env.NEXT_PUBLIC_MAINNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
