8a1f18a027d9c5f9ad7ec4f6cb9a80d73ee23377
Naman <nmndwivedi@gmail.com>
2022-04-24 00:40:53 +0200

    bidwatcher marketplace

--- bw-client/test/sample-test.js
+++ bw-client/test/sample-test.js
@@ -2,18 +2,43 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+  describe("BidwatcherMarket", function() {
+    it("Should create and execute market sales", async function() {
+      /* deploy the marketplace */
+      const BidwatcherMarketplace = await ethers.getContractFactory("BidwatcherMarketplace")
+      const bidwatcherMarketplace = await BidwatcherMarketplace.deploy()
+      await bidwatcherMarketplace.deployed()
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+      let listingPrice = await bidwatcherMarketplace.getListingPrice()
+      listingPrice = listingPrice.toString()
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+      const auctionPrice = ethers.utils.parseUnits('1', 'ether')
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+      /* create two tokens */
+      await bidwatcherMarketplace.createToken("https://www.mytokenlocation.com", auctionPrice, { value: listingPrice })
+      await bidwatcherMarketplace.createToken("https://www.mytokenlocation2.com", auctionPrice, { value: listingPrice })
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+      const [_, buyerAddress] = await ethers.getSigners()
+
+      /* execute sale of token to another user */
+      await bidwatcherMarketplace.connect(buyerAddress).createMarketSale(1, { value: auctionPrice })
+
+      /* resell a token */
+      await bidwatcherMarketplace.connect(buyerAddress).resellToken(1, auctionPrice, { value: listingPrice })
+
+      /* query for and return the unsold items */
+      items = await bidwatcherMarketplace.fetchMarketItems()
+      items = await Promise.all(items.map(async i => {
+        const tokenUri = await bidwatcherMarketplace.tokenURI(i.tokenId)
+        let item = {
+          price: i.price.toString(),
+          tokenId: i.tokenId.toString(),
+          seller: i.seller,
+          owner: i.owner,
+          tokenUri
+        }
+        return item
+      }))
+      console.log('items: ', items)
+    })
+  })});
