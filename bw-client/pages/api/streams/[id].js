44e97c93dc4550433430799903da76712344c578
Naman <nmndwivedi@gmail.com>
2022-04-24 08:53:06 +0200

    final

--- bw-client/pages/api/streams/[id].js
+++ bw-client/pages/api/streams/[id].js
@@ -1,5 +1,71 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const streamId = req.query.id;
+    const userAddress = req.body && req.body.userAddress;
+
+    try {
+      const doesRequesterOwnNFT = await axios.post(
+        "https://api.thegraph.com/subgraphs/name/nmndwivedi/bidwatcher",
+        {
+          query: `{
+                            tokens {
+                              tokenId
+                              bidders {
+                                id
+                              }
+                            }
+                          }`,
+        },
+        {
+          headers: {
+            "content-type": "application/json",
+          },
+        }
+      );
+      // res.end({data: doesRequesterOwnNFT.data});
+
+      if (doesRequesterOwnNFT && doesRequesterOwnNFT.data) {
+        const searchToken = (t) => {
+          return t.tokenId === streamId;
+        };
+
+        const searchedToken =
+          doesRequesterOwnNFT.data.data.tokens.find(searchToken);
+
+        const checkUsername = (obj) => {
+          return (
+            obj.id.toLowerCase().trim().slice(0, 10) ===
+            userAddress.toLowerCase().trim().slice(0, 10)
+          );
+        };
+
+        const valid = searchedToken.bidders.some(checkUsername);
+
+        if (valid) {
+          res.statusCode = 200;
+          res.json({
+            livestreamUrl:
+              "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8",
+          });
+        } else {
+          res.statusCode = 403;
+          res.json({
+            error: "You do not own this token",
+          });
+        }
+      } else {
+        res.statusCode = 403;
+        res.json({
+          error: "You do not own this token",
+        });
+      }
+      //    res.end({data: "You do not own this token"});
+    } catch (error) {
+      res.statusCode = 500;
+      console.log(error);
+    }
+  }
 }
--- bw-client/pages/watch/[id].js
+++ bw-client/pages/watch/[id].js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import axios from "axios";
+import Web3Modal from 'web3modal'
+import { ethers } from 'ethers'
 
 const Watch = () => {
   const router = useRouter();
   const { id } = router.query;
+  const videoRef = useRef(null);
+  const [url, setUrl] = useState(null);
 
-  return <div>Watch {id}</div>;
+  async function loadStream() {
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+    const res = await axios.post("/api/streams/"+id, {userAddress: provider.connection}, {headers: {'content-type': 'application/json'}})
+    console.log(res);
+  }
+
+  useEffect(() => {
+    // loadStream();
+
+    if (videoRef.current) {
+      videojs(videoRef.current, {
+        autoplay: true,
+        muted: true,
+        sources: [
+          {
+            src: "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8",
+            type: "application/x-mpegURL",
+          },
+        ],
+      });
+    }
+  }, []);
+
+  return (
+    <div className="p-24 flex flex-col items-center space-y-8">
+    <div className="font-semibold">Livestreaming for NFT Id: {id}</div>
+      <div className="flex relative rounded-3xl items-center justify-center h-96 overflow-hidden">
+        <video
+          controls
+          ref={videoRef}
+          className="video-js absolute z-10 min-h-full max-w-none h-full object-center object-cover"
+        ></video>
+      </div>
+    </div>
+  );
 };
 
 export default Watch;
